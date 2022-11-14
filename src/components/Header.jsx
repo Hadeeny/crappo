f2cb5eb5de52cb77bf217bd313732a5dab70ce6a
hadeeny <oluwasegunadeniyi064@gmail.com>
2022-11-14 14:34:09 +0100

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,58 +1,66 @@
 import { useState } from "react";
-import { motion } from "framer-motion";
 import logo from "../Crypto images/logo.svg";
-
+import { motion } from "framer-motion";
 
 const Header = () => {
   const [showNav, setShowNav] = useState(false);
-  
-  // const itemVariants = Variant({})
-  const itemVariants = []
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 1, y: 0, transition: { duration: 0.2 } },
+  };
+
   return (
     <motion.header
+      initial={true}
+      animate={showNav ? "open" : "closed"}
       className={`${
         showNav
           ? "h-[100vh] bg-[#0D0D2B] fixed w-full"
-          : "w-full py-4 lg:py-0 backdrop-blur-xl fixed lg:top-0"
+          : "w-full py-4 lg:py-0 bg-[#0D0D2B] fixed lg:top-0"
       }`}
     >
       <motion.nav
         className={`${
           showNav
             ? "h-[70%] flex flex-col-reverse py-4 justify-between"
-            : "w-11/12 mx-auto flex flex-row lg:py-4 lg:items-center justify-between"
+            : "w-11/12 mx-auto flex flex-row lg:py-4 lg:items-center items-center justify-between"
         } `}
       >
-        <motion.div animate={{x:20}} className={`${showNav && "hidden"}`}>
+        <div className={`${showNav && "hidden"}`}>
           <img className="cursor-pointer" src={logo} alt="logo" />
-        </motion.div>
+        </div>
         <motion.div
           className={`${
             !showNav && "hidden"
-          } py-8 lg:py-4 flex flex-col gap-y-8 items-center justify-center lg:space-x-14 lg:flex lg:flex-row lg:justify-between`}
+          } py-8 lg:py-4 flex flex-col gap-y-8 items-center justify-center  lg:space-x-14 lg:flex lg:flex-row lg:justify-between`}
         >
           <motion.ul
-          variants={{
-            open: {
-              clipPath: "inset(0% 0% 0% 0% round 10px)",
-              transition: {
-                type: "spring",
-                bounce: 0,
-                duration: 0.7,
-                delayChildren: 0.3,
-                staggerChildren: 0.05
-              }
-            },
-            closed: {
-              clipPath: "inset(10% 50% 90% 50% round 10px)",
-              transition: {
-                type: "spring",
-                bounce: 0,
-                duration: 0.3
-              }
-            }
-          }} 
-          className="flex flex-col gap-y-8 lg:space-x-8 lg:flex lg:flex-row lg:justify-between">
+            variants={{
+              open: {
+                clipPath: "inset(0% 0% 0% 0% round 10px)",
+                transition: {
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.7,
+                  delayChildren: 0.3,
+                  staggerChildren: 0.05,
+                },
+              },
+              closed: {
+                clipPath: "inset(10% 50% 90% 50% round 10px)",
+                transition: {
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.3,
+                },
+              },
+            }}
+            className="flex flex-col gap-y-8 lg:space-x-8 lg:flex lg:flex-row lg:justify-between"
+          >
             <motion.li variants={itemVariants}>
               <a className="text-3xl lg:text-[1em]" href="#">
                 Home
@@ -62,7 +70,7 @@ const Header = () => {
               <a className="text-3xl lg:text-[1em]" href="#">
                 About
               </a>
-            </motion.li >
+            </motion.li>
             <motion.li variants={itemVariants}>
               <a className="text-3xl lg:text-[1em]" href="#">
                 Service
@@ -74,28 +82,29 @@ const Header = () => {
               </a>
             </motion.li>
           </motion.ul>
-          <motion.ul
-          variants={{
-            open: {
-              clipPath: "inset(0% 0% 0% 0% round 10px)",
-              transition: {
-                type: "spring",
-                bounce: 0,
-                duration: 0.7,
-                delayChildren: 0.3,
-                staggerChildren: 0.05
-              }
-            },
-            closed: {
-              clipPath: "inset(10% 50% 90% 50% round 10px)",
-              transition: {
-                type: "spring",
-                bounce: 0,
-                duration: 0.3
-              }
-            }
-          }} 
-          className="flex gap-x-5 lg:space-x-6">
+          <ul
+            variants={{
+              open: {
+                clipPath: "inset(0% 0% 0% 0% round 10px)",
+                transition: {
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.7,
+                  delayChildren: 0.3,
+                  staggerChildren: 0.05,
+                },
+              },
+              closed: {
+                clipPath: "inset(10% 50% 90% 50% round 10px)",
+                transition: {
+                  type: "spring",
+                  bounce: 0,
+                  duration: 0.3,
+                },
+              },
+            }}
+            className="flex gap-x-5 lg:space-x-6"
+          >
             <motion.li variants={itemVariants}>
               <a className="text-3xl lg:text-[1em]" href="#">
                 Login
@@ -109,18 +118,18 @@ const Header = () => {
                 Register
               </a>
             </motion.li>
-          </motion.ul>
+          </ul>
         </motion.div>
-        <motion.div
+        <div
           onClick={() => {
             setShowNav(!showNav);
           }}
-          className={`block lg:hidden text-4xl float-right`}
+          className={`flex items-end px-4 flex-col lg:hidden text-4xl`}
         >
-          <div className='bg-white w-8 h-1'></div>
-          <div className='bg-white w-8 h-1 my-1'></div>
-          <div className='bg-white w-8 h-1'></div>
-        </motion.div>
+          <div className="bg-white w-8 h-[3px]"></div>
+          <div className="bg-white my-2 w-8 h-[3px]"></div>
+          <div className="bg-white w-8 h-[3px]"></div>
+        </div>
       </motion.nav>
     </motion.header>
   );
